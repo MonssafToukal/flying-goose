cbcccd72c8c58efbee8613a522675b5b10900c13
Monssaf Toukal <tmonssaf@gmail.com>
2026-01-23 22:58:54 -0500

    compute the rook masks at compile time

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,20 @@ pub mod movement;
 pub mod types;
 
 use board::{Board, fen::FenError, types::{Files, Ranks, Sides, SquareCoord}};
-use movement::sliders::generate_rook_occupancy_masks;
-use types::print_bb;
+use movement::sliders::ROOK_OCCUPANCY_MASKS;
+use types::{SQUARE_MASKS, print_bb};
 
 fn main() -> Result<(), FenError> {
-    let rook_occupancy_mask = generate_rook_occupancy_masks();
-    let square = SquareCoord{
-        file: Files::D,
-        rank: Ranks::R4,
-    };
-    print_bb(rook_occupancy_mask[square.to_usize()]);
+    // let square = SquareCoord{
+    //     file: Files::D,
+    //     rank: Ranks::R4,
+    // };
+    // print_bb(ROOK_OCCUPANCY_MASKS[square.to_usize()]);
+    // print_bb(SQUARE_MASKS[square.to_usize()]);
+
+    ROOK_OCCUPANCY_MASKS.iter().for_each(|bb| {
+        print_bb(*bb);
+    });
 
     Ok(())
 }
--- src/movement/sliders.rs
+++ src/movement/sliders.rs
@@ -5,25 +5,29 @@ use crate::{
     types::{BitBoard, NumOf},
 };
 
-pub fn generate_rook_occupancy_masks() -> [BitBoard; NumOf::SQUARES] {
+pub const ROOK_OCCUPANCY_MASKS:[BitBoard; NumOf::SQUARES] = generate_rook_occupancy_masks();
+
+// To make this a const function, I need to hardcode some values unfortunately
+const fn generate_rook_occupancy_masks() -> [BitBoard; NumOf::SQUARES] {
     let mut rook_occupancy_masks: [BitBoard; NumOf::SQUARES] = [EMPTY_BITBOARD; NumOf::SQUARES];
-    let nb_files: u8 = u8::try_from(NumOf::FILES).unwrap();
-    for rank in Ranks::R1 as u8..Ranks::R8 as u8 {
-        for file in Files::A as u8..Files::H as u8 {
-            let current_square = SquareCoord {
-                file: Files::try_from(file).unwrap(),
-                rank: Ranks::try_from(rank).unwrap(),
-            };
-            rook_occupancy_masks[current_square.to_usize()] = get_occupancy_mask(current_square);
-        }
+    let nb_files: u8 = 8;
+    let nb_ranks: u8 = 8;
+    let mut rank:u8 = 0;
+    let mut file:u8 = 0;
+    while rank < nb_ranks {
+        file = 0;
+        while file < nb_files {
+            let current_square: usize = file as usize + (rank * nb_files) as usize;
+                rook_occupancy_masks[current_square] = get_occupancy_mask(rank, file);
+            file +=1;
+        } 
+            rank +=1;
     }
     rook_occupancy_masks
 }
 
-fn get_occupancy_mask(initial_square: SquareCoord) -> BitBoard {
-    let nb_files: u8 = u8::try_from(NumOf::FILES).unwrap();
-    let initial_file = initial_square.file as u8;
-    let initial_rank = initial_square.rank as u8;
+const fn get_occupancy_mask(initial_rank: u8, initial_file: u8) -> BitBoard {
+    let nb_files: u8 = 8;
     let mut occupancy_mask = EMPTY_BITBOARD;
     let mut right_file = initial_file + 1;
     // Going in the right direction
