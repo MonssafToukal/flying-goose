78bd011627c9fcd5bdfa067b143252b1848047b4
Monssaf Toukal <tmonssaf@gmail.com>
2026-07-02 14:25:45 -0400

    feat(magic): printing bits set for magics

--- src/bin/magic-gen/main.rs
+++ src/bin/magic-gen/main.rs
@@ -8,7 +8,9 @@ use crate::magic::get_rook_magics;
 
 fn main() {
     let (magics, lut) = get_rook_magics();
-    magics.iter().zip(lut).for_each(|(m, bb)| {
-        println!("Magic: {}\n bitboard: ", m);
-        print_bb(bb)});
+    for (i, magic_entry) in magics.iter().enumerate() {
+        println!("Square {i}");
+        println!("{magic_entry}");
+        println!("Bits set for magic: {}", magic_entry.number.count_ones());
+    }
 }
